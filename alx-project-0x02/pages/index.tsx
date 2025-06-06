066d1a55cad38156285f61870ecb478b5830f1c8
TheMSFlow <148035422+TheMSFlow@users.noreply.github.com>
2025-06-06 15:02:27 +0100

    Update index.tsx

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,18 +1,6 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import Header from "@/components/layout/Header";
+import Header  from '../components/layout/Header';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
+const Home:React.FC = () => {
   return (
     <>
       <Header />
